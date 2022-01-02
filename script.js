5d95094f93502e078a055b7b0d66183ac85e31da
snailman06 <96637190+snailman06@users.noreply.github.com>
2022-01-02 10:44:40 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+/*principal = ammount put in
+ rate = interest rate
+ years = term of loan
+ interest = principal multiplied by term of loan multiplied by rate divided by 100
+ year = target date
+ result = output*/
+function compute() {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    var result = document.getElementById("result");
+    if (principal <= "0") {
+        alert('Enter a positive number');
+        document.getElementById("principal").focus();
+    } else {
+        result.innerHTML = '\<br\>' + 'If you deposit ' + '<mark>' + principal + '</mark>' + ',' + '\<br\>'
+            + 'at an interest rate of ' + '<mark>' + rate + '%' + '</mark>' + '.' + '\<br\>'
+            + 'You will receive an amount of ' + '<mark>' + interest + '</mark>' + ',' + '\<br\>'
+            + 'in the year ' + '<mark>' + year + '</mark>'
+    }
 }
-        
+
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval + '%';
+}
